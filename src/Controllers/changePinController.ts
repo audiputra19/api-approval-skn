1505ecc25faa2ca133c941208a37c19da9e14fd3
audi putra <audiputra19@gmail.com>
2025-05-14 16:09:17 +0700

    edit change pin controller

--- src/Controllers/changePinController.ts
+++ src/Controllers/changePinController.ts
@@ -7,7 +7,7 @@ export const ChangePinController = async (req: Request, res: Response) => {
 
     try {
         await connection.query<RowDataPacket[]>(
-            `UPDATE users SET pin = ? WHERE nip = '20002'`, [newPin]
+            `UPDATE users SET pin = ? WHERE nip = '30001'`, [newPin]
         );
 
         res.status(200).json({ message: 'Pin changed successfully' });
