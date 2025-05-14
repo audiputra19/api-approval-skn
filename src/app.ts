aaf01619c56f6afd1585023762e1f9290ca0cfa6
audi putra <audiputra19@gmail.com>
2025-05-14 15:22:34 +0700

    update ip address

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(cors({
     // origin: 'http://localhost:3000',
-    origin: 'https://approval-appskn.vercel.app',
+    origin: 'https://approval-app-skn.vercel.app',
 }))
 
 //endpoint untuk menjalankan access pin
